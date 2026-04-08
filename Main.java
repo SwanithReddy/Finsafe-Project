7b9a839ff193bede6c84be15891f3cdb28baf444
SwanithReddy <swanithreddi@gmail.com>
2026-04-08 18:29:33 +0530

    made few changes and upates

--- Main.java
+++ Main.java
@@ -1,38 +1,34 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
         System.out.println("Welcome to FinSafe Transaction Validator");
-
         System.out.print("Enter Account Holder Name: ");
-        String name = sc.nextLine();
-
+        String name=sc.nextLine();
         int initialBalance;
 
-        while (true) {
-            try {
+        while(true){
+            try{
                 System.out.print("Enter Initial Balance: ");
-                initialBalance = sc.nextInt();
+                initialBalance=sc.nextInt();
 
-                if (initialBalance < 0) {
+                if(initialBalance<0){
                     System.out.println("Balance cannot be negative. Try again.");
-                } else {
+                }else{
                     break;
                 }
 
-            } catch (Exception e) {
+            }catch(Exception e){
                 System.out.println("Invalid input! Enter a valid number.");
-                sc.nextLine(); // clear wrong input
+                sc.nextLine();
             }
         }
 
-        Account account = new Account(name, initialBalance);
-
+        Account account=new Account(name,initialBalance);
         int choice;
 
-        do {
+        do{
             System.out.println("\n===== MENU =====");
             System.out.println("1.Deposit");
             System.out.println("2.Withdraw");
@@ -42,8 +38,8 @@ public class Main {
             System.out.print("Enter your choice: ");
             choice = sc.nextInt();
 
-            try {
-                switch (choice) {
+            try{
+                switch(choice){
 
                     case 1:
                         System.out.print("Enter deposit amount: ");
@@ -73,17 +69,16 @@ public class Main {
                         System.out.println("Invalid choice! Please enter between 1 and 5.");
                 }
 
-            } catch (IllegalArgumentException e) {
+            }catch(IllegalArgumentException e){
                 System.out.println("Validation Error: " + e.getMessage());
-            } catch (InsufficientfundsException e) {
+            }catch (InsufficientfundsException e){
                 System.out.println("Transaction Error: " + e.getMessage());
-            } catch (Exception e) {
+            }catch (Exception e){
                 System.out.println("Unexpected Error: " + e.getMessage());
-                sc.nextLine(); // clear wrong input
+                sc.nextLine(); 
             }
 
-        } while (choice != 5);
-
+        }while(choice!=5);
         sc.close();
     }
 }
